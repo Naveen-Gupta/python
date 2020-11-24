88dfe4f65f440195c806ccfb4562c66f2eb1d864
Gaurav Dixit <gaurav.dixit@telus.com>
2020-11-24 22:02:20 +0530

    added string operators

---
+++ string_operators.py
@@ -0,0 +1,19 @@
+string1 = "Hello "
+string2 = "Hello "
+string3 = "Hello "
+string4 = "Hello "
+string5 = "Hello"
+
+print(string1 + string2 + string3 +string4 + string5)
+print("Hello " "Hello " "Hello " "Hello " "Hello")
+print("Hello " * 5)
+
+#print("Hello " * 5 + 4)
+print("Hello " * (5 + 4))
+print("Hello " * 5 + '4')
+
+print()
+today = "tuesday"
+print("day" in today)   # True
+print("tues" in today)  # True
+print("abc" in today)   # False
--- strings.py
+++ strings.py
@@ -10,9 +10,9 @@ print("Hello 'World'!!")
 print("Hello" + " " + "World!!")
 
 # taking input from User
-name = input("Please enter your name:")
+#name = input("Please enter your name:")
 
-print("Hello " + name, "!!")
+#print("Hello " + name, "!!")
 
 str = "Norwegian Blue"
 for i in range(0, len(str)):
@@ -34,3 +34,30 @@ print()
 print(str[-11])
 print(str[-8])
 print(str[-6])
+
+# slicing(up to but not including)
+print()
+print(str[0:6])     # Norweg
+print(str[3:5])     # we
+print(str[0:9])     # Norwegian
+print(str[:9])      # Norwegian
+print(str[10:14])   # Blue
+print(str[10:])     # Blue
+print(str[:])       # Blue
+
+print()
+print(str[-4:-2])   # Bl
+print(str[-4:12])   # Bl
+print(str[:-1])     # Norwegian Blue
+
+# step
+print()
+print(str[0:6:2])   # Nre
+print(str[0:6:3])   # Nw
+print("0,111;222!333#444"[1::4])    # ,;!#
+print(str[:-1:2])   # NreinBu
+
+#backward slicing
+print()
+print(str[14:0:-1])     # eulB naigewro
+print(str[::-1])        # eulB naigewroN
