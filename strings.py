ce8d17858791e4749ee663181a8cd126536e8ac9
Gaurav Dixit <gaurav.dixit@telus.com>
2020-11-24 16:42:59 +0530

    added string

---
+++ strings.py
@@ -0,0 +1,15 @@
+print("Hello World!!")
+
+# double quotes in string
+print('Hello "World"!!')
+
+# single quotes in string
+print("Hello 'World'!!")
+
+# string concat
+print("Hello" + " " + "World!!")
+
+# taking input from User
+name = input("Please enter your name:")
+
+print("Hello " + name, "!!")
