9ea155dba81242115f83e9385670b91e7ccaaaa1
Gaurav Dixit <gaurav.dixit@telus.com>
2020-11-24 17:49:19 +0530

    added escapechar

---
+++ escapechar.py
@@ -0,0 +1,28 @@
+splitStringByNewLine = "This string has been\nsplit over\nseveral\nlines"
+print(splitStringByNewLine)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+# or
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+# or
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
+
+anotherSplittedStringByNewLine = """This string has been
+split over
+several
+lines"""
+print(anotherSplittedStringByNewLine)
+
+
+removeSplittedStringByNewLine = """This string has been \
+split over \
+several \
+lines"""
+print(removeSplittedStringByNewLine)
+
+#print("C:\Users\timba\notes.txt")
+print("C:\\Users\\timba\\notes.txt")
+print(r"C:\Users\timba\notes.txt")
